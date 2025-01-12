f975d49113d5d5ce8e632c6ee6c0822fca9fd477
Syed Ahmed <syedahmed29@gmail.com>
2025-01-12 13:16:30 -0800

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -5,10 +5,12 @@ module "eks_cluster" {
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id          = var.vpc_id
   subnet_ids      = var.subnet_ids
-
-  manage_aws_auth = true
 
   tags = {
     Environment = var.environment
