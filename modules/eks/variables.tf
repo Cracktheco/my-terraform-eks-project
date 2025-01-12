414207ecc10e2f2f2e4e050a958b834ed601172b
Syed Ahmed <syedahmed29@gmail.com>
2025-01-11 16:13:42 -0800

    Create variables.tf

---
+++ modules/eks/variables.tf
@@ -0,0 +1,25 @@
+variable "cluster_name" {
+  type        = string
+  description = "Name for the EKS cluster"
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "Kubernetes version for the EKS cluster"
+  default     = "1.24"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the cluster"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnets to place the EKS cluster"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+}
