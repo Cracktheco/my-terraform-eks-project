f6ea89115949a7b6eed178a96eaa6eb0b1fff1a1
Syed Ahmed <syedahmed29@gmail.com>
2025-01-11 16:09:22 -0800

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,39 @@
+/*
+  This module creates a VPC with public and private subnets.
+  Adjust variables as needed for your environment.
+*/
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+# Data source to fetch available AZs
+data "aws_availability_zones" "available" {}
+
+# Output the VPC ID
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
