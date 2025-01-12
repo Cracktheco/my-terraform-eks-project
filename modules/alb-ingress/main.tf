b99e1db24f4f7675383384ee6119cd6e81b9cb7c
Syed Ahmed <syedahmed29@gmail.com>
2025-01-11 16:21:21 -0800

    Create main.tf

---
+++ modules/alb-ingress/main.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority)
+    token                  = data.aws_eks_cluster_auth.auth.token
+  }
+}
+
+data "aws_eks_cluster_auth" "auth" {
+  name = var.cluster_name
+}
+
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  
+  values = [
+    templatefile("${path.module}/values.yaml", {
+      cluster_name = var.cluster_name
+    })
+  ]
+}
+
+output "alb_controller_name" {
+  value = helm_release.alb_controller.name
+}
