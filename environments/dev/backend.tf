fa71341b092b31f1deea6940bb51154625556e42
Syed Ahmed <syedahmed29@gmail.com>
2025-01-12 12:51:40 -0800

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "secret-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "my-terraform-locks"
